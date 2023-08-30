ac46dc9abbbd699142da7a43fc8be66dc3f5f858
Pankajlamgria <125663310+Pankajlamgria@users.noreply.github.com>
2023-08-30 23:27:41 +0530

    changed express
    code

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const connect=require('./dbs.js');
 const cors=require("cors");
 connect();
 app.use(cors());
-app.use(express.json());
+app.use(bodyParser.json()); app.use(bodyParser.urlencoded({ extended: true }));
 const port =process.env.PORT || 4000;
 
 app.use('/api/auth',require("./routes/auth.js"));
